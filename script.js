4c1f16d0301a10586b4d9fbe0ee8d0323e9885a0
youngberry1 <abdulabdul28@aol.com>
2025-05-12 13:36:20 +0000

    Refactor HTML for improved readability and update recommendation message URL

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function sendWhatsAppMessage() {
 
       function sendRecommendationMessage() {
         const friendNumber = document.getElementById('friend-number').value;
-        const message = `Hey! Check out DataDock Ghana for the best data deals! They are legit. They are not scam. Visit https://DataDockghana.com to buy data.`;
+        const message = `Hey! Check out DataDock Ghana for the best data deals! They are legit. They are not scam. Visit https://datadockghana.netlify.app/ to buy data.`;
         const whatsappUrl = `https://wa.me/${friendNumber}?text=${encodeURIComponent(message)}`;
         window.open(whatsappUrl, '_blank');
       }
