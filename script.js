626b510c031b3b9f8398cc39020ae4e96cc16f50
youngberry1 <abdulabdul28@aol.com>
2025-05-12 13:02:06 +0000

    Add initial HTML, CSS, and JavaScript for DataDock data bundle ordering platform

---
+++ script.js
@@ -0,0 +1,15 @@
+function sendWhatsAppMessage() {
+        const plan = document.getElementById('data-plan').value;
+        const phoneNumber = document.getElementById('phone-number').value;
+        const [bundle, cost] = plan.split(' - ');
+        const message = `I want to order ${bundle} for ${cost}. Bundle For This Number ${phoneNumber}.`;
+        const whatsappUrl = `https://wa.me/+233503868557?text=${encodeURIComponent(message)}`;
+        window.open(whatsappUrl, '_blank');
+      }
+
+      function sendRecommendationMessage() {
+        const friendNumber = document.getElementById('friend-number').value;
+        const message = `Hey! Check out DataDock Ghana for the best data deals! They are legit. They are not scam. Visit https://DataDockghana.com to buy data.`;
+        const whatsappUrl = `https://wa.me/${friendNumber}?text=${encodeURIComponent(message)}`;
+        window.open(whatsappUrl, '_blank');
+      }
